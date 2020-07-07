b19201f4674512d6450cafc2c7e51a4e1b1eaa9f
WINGLYst4rk <wingly205@gmail.com>
2020-07-07 08:43:15 -0300

    Resposta do desafio proposto pela coder cursos no curso de Java da udemy.

---
+++ exercicios/src/fundamentos/RespostaDesafioAritmeticos.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+public class RespostaDesafioAritmeticos {
+	
+	public static void main(String[] args) {
+		
+		double numA = Math.pow(6 * (3 + 2), 2);
+		double denA = 3 * 2;
+		
+		double numB = (1 - 5) * (2 - 7);
+		double denB = 2;
+		
+		double superiorA = numA / denA;
+		double supiriorB = Math.pow(numB / denB, 2);
+		
+		double 
+	}
+
+}
