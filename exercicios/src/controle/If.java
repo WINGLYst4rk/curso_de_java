3076b09645ae32408b95ce8d1e1027942c83b7b5
WINGLYst4rk <wingly205@gmail.com>
2020-07-24 19:39:20 -0300

    modificações com base no codigo proposto pela Cod3r cursos.

--- exercicios/src/controle/If.java
+++ exercicios/src/controle/If.java
@@ -5,23 +5,30 @@ import java.util.Scanner;
 public class If {
 	
 	public static void main(String[] args) {
+		//código base do Leonardo leitão
+		// agora vou adicionar minha versão
 		
 		Scanner en = new Scanner(System.in);
 		
 		System.out.println("Informe a média: ");
 		double media = en.nextDouble();
 		
-		if(media < 10.0 && media >= 7.0) {
+		//criando variaveis para substituir dentro dos ifs
+		boolean criterioReprovacao = media < 4.5 && media >= 0;
+		boolean criterioRecuperacao = media < 7.0 && media >= 4.5;
+		boolean criterioAprovado = media < 10.0 && media >= 7.0;
+		
+		if(criterioAprovado) {
 			System.out.println("Parabéns!");
 			System.out.println("Aprovado!");
 		}
 		
-		if(media < 7.0 && media >= 4.5) {
+		if(criterioRecuperacao) {
 			System.out.println("Recuperação!");
 			System.out.println("Não desista!");
 		}
 		
-		if(media < 4.5 && media >= 0) {
+		if(criterioReprovacao) {
 			System.out.println("Reprovado!");
 		}
 		
