79eb4ff885818f1049d31ab372436454918728ee
WINGLYst4rk <wingly205@gmail.com>
2021-01-14 21:04:15 -0300

    desafio sobre forEach e arrays

--- exercicios/src/arrays/Desafio1.java
+++ exercicios/src/arrays/Desafio1.java
@@ -14,16 +14,16 @@ public class Desafio1 {
 		double[] notas = new double[quantidadeNotas];
 
 		for (int i = 0; i < notas.length; i++) {
-			System.out.printf("digite a %dº nota ", i+1);
-			notas [i] = entrada.nextDouble();
+			System.out.printf("digite a %dº nota ", i + 1);
+			notas[i] = entrada.nextDouble();
 		}
-		
+
 		double media = 0;
-		
-		for (double nota: notas) {
+
+		for (double nota : notas) {
 			media += nota;
 		}
-		
+
 		System.out.printf("A media do alulo e: %.1f", media / quantidadeNotas);
 		System.out.printf("\nForam digitadas %d notas!", quantidadeNotas);
 
