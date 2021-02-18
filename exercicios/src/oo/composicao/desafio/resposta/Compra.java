65d974b873bc166aa4bdf3609e2414083d180ae7
WINGLYst4rk <wingly205@gmail.com>
2021-02-18 17:07:46 -0300

    mais ajustes nas classes.

--- exercicios/src/oo/composicao/desafio/resposta/Compra.java
+++ exercicios/src/oo/composicao/desafio/resposta/Compra.java
@@ -10,7 +10,13 @@ public class Compra {
 	
 	//metodo para adicionar itens
 	void adicionarItem(Produto p, int qtde) {
-		this.itens.add(new Item(p, qtde));// passadno o item e ja criando o produto para adicionar
+		this.itens.add(new Item(p, qtde));// passadno o item e criando o produto para adicionar
+	}
+	
+	//metodo para adicionar um item e criar um produto
+	void adicionarItem(String nome, double preco, int qtde) {
+		var produto = new Produto(nome, preco);// usasndo inferencia para criar um produto
+		this.itens.add(new Item(produto, qtde));
 	}
 	
 	//metodo para calcular o total de cada compra
--- exercicios/src/oo/composicao/desafio/resposta/Sistema.java
+++ exercicios/src/oo/composicao/desafio/resposta/Sistema.java
@@ -3,6 +3,6 @@ package oo.composicao.desafio.resposta;
 public class Sistema {
 
 	public static void main(String[] args) {
-		
+		 
 	}
 }
