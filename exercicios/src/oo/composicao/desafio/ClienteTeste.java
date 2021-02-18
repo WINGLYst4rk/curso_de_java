0776005204420534d369663bcda0f71a015215b6
WINGLYst4rk <wingly205@gmail.com>
2021-02-18 18:44:16 -0300

    apagando o codigo antigo para colocar minha resposta.

--- exercicios/src/oo/composicao/desafio/ClienteTeste.java
+++ exercicios/src/oo/composicao/desafio/ClienteTeste.java
@@ -2,13 +2,4 @@ package oo.composicao.desafio;
 
 public class ClienteTeste {
 
-	public static void main(String[] args) {
-		
-		Cliente cliente1 = new Cliente("Maria");
-		Compra compra1 = new Compra();
-
-		
-		
-	}
-	
 }
--- exercicios/src/oo/composicao/desafio/Item.java
+++ exercicios/src/oo/composicao/desafio/Item.java
@@ -1,15 +1,5 @@
 package oo.composicao.desafio;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Item {
 
-	Produto produto;
-	int quantidade;
-	List<Produto> produtos = new ArrayList<>();
-	
-	void adicionarProduto(Produto produto) {
-		this.produtos.add(produto);
-	}
 }
