298bb10ea166f4b750aaf68d6e69188864460593
WINGLYst4rk <wingly205@gmail.com>
2021-02-18 16:58:54 -0300

    ajustando detalhes da classe cliente

--- exercicios/src/oo/composicao/desafio/resposta/Cliente.java
+++ exercicios/src/oo/composicao/desafio/resposta/Cliente.java
@@ -5,8 +5,14 @@ import java.util.List;
 
 public class Cliente {
 
+	String nome;
 	//Lista para armazenar as compras do objeto cliente
 	List<Compra> compras = new ArrayList<>();
+	
+	//Construtor de clientes
+	Cliente(String nome){
+		this.nome = nome;
+	}
 	
 	//metodo para calcular total de todas as compras
 	double obterValorTotal() {
