5cbaba607b14ee051e14e8f30fef4989754e910b
WINGLYst4rk <wingly205@gmail.com>
2021-02-18 17:42:12 -0300

    segundo teste com valores menores

--- exercicios/src/oo/composicao/desafio/resposta/Sistema.java
+++ exercicios/src/oo/composicao/desafio/resposta/Sistema.java
@@ -6,12 +6,12 @@ public class Sistema {
 		 Cliente cliente = new Cliente("Maria Julia Moraes");
 		 
 		 Compra compra1 = new Compra();
-		 compra1.adicionarItem("Caneta", 9.67, 120);
-		 compra1.adicionarItem(new Produto("Notebook", 18890.25), 2);
+		 compra1.adicionarItem("Caneta", 1, 120);
+		 compra1.adicionarItem(new Produto("Notebook", 1000), 2);
 		 
 		 Compra compra2 = new Compra();
-		 compra2.adicionarItem("caderno", 10.90, 20);
-		 compra2.adicionarItem(new Produto("Impressora", 985.70), 1);
+		 compra2.adicionarItem("caderno", 10, 20);
+		 compra2.adicionarItem(new Produto("Impressora", 1000), 1);
 		 
 		 cliente.compras.add(compra1);
 		 cliente.compras.add(compra2);
